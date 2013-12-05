0890e534e7e65956abaa3b682b7bdef4ac1b700f
bernhardschaefer <bschaefer87@gmx.net>
2013-12-05 11:18:56 +0100

    decode spotlight resource uris

--- core/src/main/java/org/dbpedia/spotlight/graphdb/DBpediaSense.java
+++ core/src/main/java/org/dbpedia/spotlight/graphdb/DBpediaSense.java
@@ -2,6 +2,7 @@ package org.dbpedia.spotlight.graphdb;
 
 import org.dbpedia.spotlight.model.DBpediaResource;
 
+import de.unima.dws.dbpediagraph.graph.UriTransformer;
 import de.unima.dws.dbpediagraph.model.DefaultSense;
 import de.unima.dws.dbpediagraph.model.Sense;
 
@@ -16,19 +17,8 @@ public class DBpediaSense extends DefaultSense implements Sense {
 	private final DBpediaResource resource;
 
 	public DBpediaSense(DBpediaResource resource) {
-		// Spotlight resources are Url encoded, DBpedia dumps are partly encoded.
-
-		// Spotlight --> DBpedia Dump
-
-		// Napol%C3%A9on_%281955_film%29 --> Napol%C3%A9on_(1955_film)
-		// Wolfgang_Graf_von_Bl%C3%BCcher --> Wolfgang_Graf_von_Blücher
-
-		// Other examples
-		// Sandra_Day_O%27Connor --> Sandra_Day_O'Connor
-		// Goal%21_(film) --> Goal!_(film)
-
-		// TODO think about proper transformation and get rid of ugly hack
-		super(resource.getFullUri().replace("%28", "(").replace("%29", ")").replace("%27", "'").replace("%21", "!"));
+		// Spotlight resources are URL encoded and need to be decoded
+		super(UriTransformer.decode(resource.getFullUri()));
 		this.resource = resource;
 	}
 
