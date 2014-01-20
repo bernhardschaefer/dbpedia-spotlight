7074301a01222abd00d69c19a8271606d8b61156
bernhardschaefer <bschaefer87@gmx.net>
2014-01-20 20:12:01 +0100

    very verboose candidate and score logging for error analysis

--- core/src/main/scala/org/dbpedia/spotlight/disambiguate/mixtures/UnweightedMixture.scala
+++ core/src/main/scala/org/dbpedia/spotlight/disambiguate/mixtures/UnweightedMixture.scala
@@ -15,27 +15,15 @@ class UnweightedMixture(features: Set[String]) extends Mixture(1) {
   def getScore(occurrence: DBpediaResourceOccurrence): Double = {
     val fs = occurrence.features.values.filter({ f: Feature => features.contains(f.featureName) }).map(_.value.asInstanceOf[Double])
     val score = fs.foldLeft(0.0)((x: Double, y: Double) => if(x == -inf || y == -inf) -inf else x + y)
-    
-    // Set("P(e)", "P(c|e)", "P(s|e)")
-    val p_se = occurrence.featureValue("P(s|e)") match { 
-      case Some(p) => p.asInstanceOf[Double]
-      case None => 0.0
-    }
-    
-    val p_e = occurrence.featureValue("P(e)") match { 
-      case Some(p) => p.asInstanceOf[Double]
-      case None => 0.0
-    }
-    
-    val p_ce = occurrence.featureValue("P(c|e)") match { 
-      case Some(p) => p.asInstanceOf[Double]
-      case None => 0.0
-    }
-    
-    //TODO remove verboose score logging again
-    SpotlightLog.debug(this.getClass, "%s --> %s [sim: %.3f, P(s|e): %.3f, P(c|e): %.3f, P(e): %.3f]", 
-    		occurrence.textOffset + ":" + occurrence.surfaceForm.name, occurrence.resource.uri, score, p_se, p_ce, p_e)
-        
+
+    //TODO remove verbose score logging again
+    val fsMap = features.foldLeft(Map[String, Double]())(
+      (acc, f) => acc + (f -> (occurrence.featureValue[Double](f).getOrElse(Double.NaN))))
+
+    SpotlightLog.debug(this.getClass, "%s --> %s [sim: %.3f, P(s|e): %.3f, P(c|e): %.3f, P(e): %.3f]",
+      occurrence.textOffset + ":" + occurrence.surfaceForm.name, occurrence.resource.uri, score,
+      fsMap("P(s|e)"), fsMap("P(c|e)"), fsMap("P(e)"))
+
     score
   }
 
