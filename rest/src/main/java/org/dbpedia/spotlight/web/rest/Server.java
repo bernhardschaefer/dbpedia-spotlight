97f98f38260e37c364289c87bd961866bf3cd747
bernhardschaefer <bschaefer87@gmx.net>
2014-03-15 14:09:33 +0100

    change to 30 min transaction timeout

--- rest/src/main/java/org/dbpedia/spotlight/web/rest/Server.java
+++ rest/src/main/java/org/dbpedia/spotlight/web/rest/Server.java
@@ -111,7 +111,7 @@ public class Server {
 
 
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(serverURI, initParams);
-        threadSelector.setTransactionTimeout(0);
+        threadSelector.setTransactionTimeout(30 * 60 * 1000); // 30 minutes
         threadSelector.start();
 
         System.err.println("Server started in " + System.getProperty("user.dir") + " listening on " + serverURI);
