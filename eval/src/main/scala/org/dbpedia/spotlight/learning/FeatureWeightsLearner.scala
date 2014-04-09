0fb545a1d4648018dc0be70c83d8c1448334c042
bernhardschaefer <bschaefer87@gmx.net>
2014-04-09 16:24:12 +0200

    [WIP] learn feature weights

--- eval/src/main/scala/org/dbpedia/spotlight/learning/FeatureWeightsLearner.scala
+++ eval/src/main/scala/org/dbpedia/spotlight/learning/FeatureWeightsLearner.scala
@@ -28,41 +28,43 @@ object FeatureWeightsLearner {
   }
 
   def learnWeights(corpus: AnnotatedTextSource, disambiguator: ParagraphDisambiguator, k: Int) = {
+    // for each document of gs
     // 1. get all gold standard spots
     // 2. get bestK for each spot
     // 3. for each gs spot
     //    3.1. bestK: get weights for highest ranked incorrect entity
     //    3.2. bestK: get weights for correct gs entity
-    
-    val gsOccs: List[DBpediaResourceOccurrence] = corpus.foldLeft(ListBuffer[DBpediaResourceOccurrence]())((acc, paragraph) => acc ++= paragraph.occurrences).toList
-    val spots: List[SurfaceFormOccurrence] = gsOccs.map(occ =>
-      new SurfaceFormOccurrence(occ.surfaceForm, occ.context, occ.textOffset, occ.provenance, -1))
-    
-    val globalParagraph = Factory.paragraph().from(spots)
-    val bestK: Map[SurfaceFormOccurrence, List[DBpediaResourceOccurrence]] = disambiguator.bestK(globalParagraph, k)
-    
-    gsOccs.foreach(gsOcc => {
-      val spot = spots.find(sfo => sfo.surfaceForm.equals(gsOcc.surfaceForm) && sfo.textOffset.equals(gsOcc.textOffset)).get
-      val occs = bestK(spot).sortBy(_.textOffset)
 
-      occs.foreach(occ => {
-        SpotlightLog.debug(this.getClass, "%s --> %s [%s]",
-          occ.textOffset + ":" + occ.surfaceForm.name, occ.resource.uri, formatWeights(occ))
+    corpus.foreach(doc => {
+      val spots: List[SurfaceFormOccurrence] = doc.occurrences.map(occ =>
+        new SurfaceFormOccurrence(occ.surfaceForm, occ.context, occ.textOffset, occ.provenance, -1))
+
+      val globalParagraph = Factory.paragraph().from(spots)
+      val bestK: Map[SurfaceFormOccurrence, List[DBpediaResourceOccurrence]] = disambiguator.bestK(globalParagraph, k)
+
+      doc.occurrences.foreach(gsOcc => {
+        val spot = spots.find(sfo => sfo.surfaceForm.equals(gsOcc.surfaceForm) && sfo.textOffset.equals(gsOcc.textOffset)).get
+        val occs = bestK(spot).sortBy(_.textOffset)
+
+        occs.foreach(occ => {
+          SpotlightLog.debug(this.getClass, "%s --> %s [%s]",
+            occ.textOffset + ":" + occ.surfaceForm.name, occ.resource.uri, formatWeights(occ))
+        })
+
+        val incorrectEntity = occs.find(!_.resource.equals(gsOcc.resource))
+        val correctEntity = occs.find(_.resource.equals(gsOcc.resource))
+
+        incorrectEntity match {
+          case Some(occ) => printf("0%s%n", formatWeights(occ))
+          case None => printf("No incorrect entity found for %s[pos %d]%n", gsOcc.surfaceForm.name, gsOcc.textOffset)
+        }
+
+        correctEntity match {
+          case Some(occ) => printf("1%s%n", formatWeights(occ))
+          case None => printf("No correct entity found for %s[pos %d]%n", gsOcc.surfaceForm.name, gsOcc.textOffset)
+        }
+
       })
-
-      val incorrectEntity = occs.find(!_.resource.equals(gsOcc.resource))
-      val correctEntity = occs.find(_.resource.equals(gsOcc.resource))
-
-      incorrectEntity match {
-        case Some(occ) => printf("0%s%n", formatWeights(occ))
-        case None => printf("No incorrect entity found for %s[pos %d]%n", gsOcc.surfaceForm.name, gsOcc.textOffset)
-      }
-
-      correctEntity match {
-        case Some(occ) => printf("1%s%n", formatWeights(occ))
-        case None => printf("No correct entity found for %s[pos %d]%n", gsOcc.surfaceForm.name, gsOcc.textOffset)
-      }
-
     })
 
     //TODO use breeze linear regression
