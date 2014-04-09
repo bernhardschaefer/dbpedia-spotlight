4400e0885cdc8d30dd37ae4612dbe2ab4c85c023
bernhardschaefer <bschaefer87@gmx.net>
2014-04-09 10:05:45 +0200

    [WIP] learn feature weights for disambiguator

---
+++ eval/src/main/scala/org/dbpedia/spotlight/learning/FeatureWeightsLearner.scala
@@ -0,0 +1,65 @@
+package org.dbpedia.spotlight.learning
+
+import java.io.File
+import scala.collection.mutable.ListBuffer
+import org.dbpedia.spotlight.corpus.AidaCorpus
+import org.dbpedia.spotlight.db.SpotlightModel
+import org.dbpedia.spotlight.disambiguate.ParagraphDisambiguator
+import org.dbpedia.spotlight.io.AnnotatedTextSource
+import org.dbpedia.spotlight.log.SpotlightLog
+import org.dbpedia.spotlight.model.DBpediaResourceOccurrence
+import org.dbpedia.spotlight.model.Factory
+import org.dbpedia.spotlight.model.SpotlightConfiguration.DisambiguationPolicy
+import org.dbpedia.spotlight.model.SurfaceFormOccurrence
+import org.dbpedia.spotlight.model.DBpediaResourceOccurrence
+
+object FeatureWeightsLearner {
+
+  def main(args: Array[String]) {
+    val corpus = AidaCorpus.fromFile(new File(args(1))) // /path/to/AIDA-YAGO2-dataset.tsv
+    val db = SpotlightModel.fromFolder(new File(args(0)))
+    val mergedDisambiguator = db.disambiguators.get(DisambiguationPolicy.Merged)
+    learnWeights(corpus, mergedDisambiguator.disambiguator)
+  }
+
+  def learnWeights(corpus: AnnotatedTextSource, disambiguator: ParagraphDisambiguator) = {
+    // 1. get all gold standard spots
+    val gsOccs: List[DBpediaResourceOccurrence] = corpus.foldLeft(ListBuffer[DBpediaResourceOccurrence]())((acc, paragraph) => acc ++= paragraph.occurrences).toList
+    val spots: List[SurfaceFormOccurrence] = gsOccs.map(occ =>
+      new SurfaceFormOccurrence(occ.surfaceForm, occ.context, occ.textOffset, occ.provenance))
+
+    // 2. get bestK for each spot
+    val globalParagraph = Factory.paragraph().from(spots)
+    val bestK: Map[SurfaceFormOccurrence, List[DBpediaResourceOccurrence]] = disambiguator.bestK(globalParagraph, 20)
+
+    // 3. for each gs spot
+    //    3.1. bestK: get weights for highest ranked incorrect entity
+    //    3.2. bestK: get weights for correct gs entity
+    gsOccs.foreach(gsOcc => {
+      val spot = spots.find(sfo => sfo.surfaceForm.equals(gsOcc.surfaceForm) && sfo.textOffset.equals(gsOcc.textOffset)).get
+      val occs = bestK(spot).sortBy(_.textOffset)
+
+      val incorrectEntity = occs.find(!_.resource.equals(gsOcc.resource))
+      val correctEntity = occs.find(_.resource.equals(gsOcc.resource))
+
+      incorrectEntity match {
+        case Some(occ) => printf("0%s%n", formatWeights(occ))
+        case None => printf("No incorrect entity found for %s[pos %d]%n", gsOcc.surfaceForm.name, gsOcc.textOffset)
+      }
+
+      correctEntity match {
+        case Some(occ) => printf("1%s%n", formatWeights(occ))
+        case None => printf("No correct entity found for %s[pos %d]%n", gsOcc.surfaceForm.name, gsOcc.textOffset)
+      }
+
+    })
+  }
+
+  def formatWeights(occ: DBpediaResourceOccurrence): String = {
+    "\t%f\t%f\t%f\t%f".format(
+      occ.featureValue[Double]("P(e)").get,
+      occ.featureValue[Double]("P(c|e)").get,
+      occ.featureValue[Double]("P(s|e)").get,
+      occ.featureValue[Double]("P(g|e)").get)
+  }
+}
