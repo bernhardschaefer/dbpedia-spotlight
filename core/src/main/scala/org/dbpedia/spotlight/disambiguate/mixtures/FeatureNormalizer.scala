a567719a342098219bd45eb2ce7f27363ea74c93
bernhardschaefer <bschaefer87@gmx.net>
2014-04-18 13:21:05 +0200

    [QuickFix] use fallback since NIL Entity has no P(s|e) without spotting

--- core/src/main/scala/org/dbpedia/spotlight/disambiguate/mixtures/FeatureNormalizer.scala
+++ core/src/main/scala/org/dbpedia/spotlight/disambiguate/mixtures/FeatureNormalizer.scala
@@ -1,8 +1,7 @@
 package org.dbpedia.spotlight.disambiguate.mixtures
 
-import org.dbpedia.spotlight.model.DBpediaResourceOccurrence
-import breeze.linalg.DenseVector
 import org.dbpedia.spotlight.graphdb.DBMergedDisambiguator
+import org.dbpedia.spotlight.model.DBpediaResourceOccurrence
 
 trait FeatureNormalizer {
   def fromOcc(occ: DBpediaResourceOccurrence): List[(String, Double)]
@@ -10,7 +9,9 @@ trait FeatureNormalizer {
 
 class DefaultFeatureNormalizer extends FeatureNormalizer {
   def fromOcc(occ: DBpediaResourceOccurrence): List[(String, Double)] = {
-    List("P(e)", "P(c|e)", "P(s|e)").map(f => (f, occ.featureValue[Double](f).getOrElse(0.0))) // 0.0 fallback since NIL Entity has no P(s|e)
+    // QuickFix: use fallback since NIL Entity has no P(s|e) without spotting
+    // scores are logarithmic scale, -1000 is very low 
+    List("P(e)", "P(c|e)", "P(s|e)").map(f => (f, occ.featureValue[Double](f).getOrElse(-1000.0)))
   }
 }
 
@@ -29,7 +30,7 @@ class MergedSemiLinearFeatureNormalizer extends FeatureNormalizer {
       ("P(c|e)", occ.contextualScore), // feature value "P(c|e)" is too small for exp, e.g. exp(-434)== 0 with double precision
       ("P(s|e)", occ.featureValue[Double]("P(s|e)") match {
         case Some(score) => Math.exp(score)
-        case None => 0.0 // NIL Entity has no P(s|e)
+        case None => 0.0 // QuickFix: use fallback since NIL Entity has no P(s|e) without spotting
       }),
       (DBMergedDisambiguator.PGraph, occ.featureValue[Double](DBMergedDisambiguator.PGraph).getOrElse(0.0)))
   }
